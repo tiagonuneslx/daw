1e5364799a1c71a1a0030f33a7d19693dcaef60e
Tiago Nunes <tiago.nunes.lx@gmail.com>
2020-09-18 16:16:27 +0100

    Change .env and .gitignore

--- LAB11/vendor/composer/autoload_real.php
+++ LAB11/vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitb96eb3ca8b535a87944e62a396f05681
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
